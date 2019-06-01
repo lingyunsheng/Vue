5c8741eaac0cc90f0194a15d4ffcacddd1a88262
lingyunsheng <1841046698@qq.com>
2019-06-01 22:48:16 +0800

    update

--- xiangwushuo/pages/RedFlower/RedFlower.js
+++ xiangwushuo/pages/RedFlower/RedFlower.js
@@ -13,30 +13,38 @@ Page({
     }
 
     ],
-    taskList:[
+    taskList1: [
       {
-        src:'../../images/Screenshot_2019_0601_154631.png',
-        detail:'好友签到',
-        btn:'最高30朵'
-      },
-      {
-        src:'../../images/Screenshot_2019_0601_154738.png',
-        detail:'邀请新人',
-        btn:'+50~80朵'
-      },
-      {
-        src:'../../images/Screenshot_2019_0601_154755.png',
-        detail:'送物赚花',
-        btn:'+10朵'
-      },
-      {
-        src:'../../images/Screenshot_2019_0601_154807.png',
-        detail:'视频红花',
-        btn:'最高10朵'
+        src: '../../images/Screenshot_2019_0601_154631.png',
+        detail: '好友签到',
+        btn: '最高30朵'
       }
-    ]
-
-
+    ],
+    taskList2: [
+      {
+        src: '../../images/Screenshot_2019_0601_154738.png',
+        detail: '邀请新人',
+        btn: '+50~80朵'
+      },
+    ],
+    taskList3: [
+      {
+        src: '../../images/Screenshot_2019_0601_154755.png',
+        detail: '送物赚花',
+        btn: '+10朵'
+      },
+    ],
+    taskList4: [
+      {
+        src: '../../images/Screenshot_2019_0601_154807.png',
+        detail: '视频红花',
+        btn: '最高10朵'
+      }
+    ],
+    show:false
+    
+  
+   
 
   },
 
@@ -44,8 +52,81 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // wx.showShareMenu({
+    //   withShareTicket: true,
+    //   success(res) {
+    //     console('成功的分享');
+    //     console('分享' + res);
+    //   },
+    //   fail(err) {
+    //     console.log('分享失败');
+    //   }
+    // })
+    this.otehrApp();
+    this.showToast();
+   
   },
+  rule(){
+    this.setData({
+      show: true
+    })
+  },
+  close: function() {
+    this.setData({
+      show: false,
+    })
+  },
+
+  touchMove: function() {
+  },
+
+  maskTouchMove: function() {
+  },
+  showToast(){
+    wx.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 2000
+    })
+   
+  },
+  otehrApp(){
+    wx.navigateToMiniProgram({
+      appId: 'wxeae00915600c9e68',
+      path: '../index/index',
+      extraData: {
+        foo: 'bar'
+      },
+      envVersion: 'develop',
+      success(res) {
+        // 打开成功
+      },
+      fail(res){
+        console.log('打开失败')
+      }
+    })
+  },
+  // onShareAppMessage() {
+  //   return {
+  //     title: '享物说一个好玩的小程序',
+  //     path: '../RedFlower/RedFlower',
+  //     success(res) {
+  //       console.log(res.shareTickets[0])
+  //       // console.log
+  //       wx.getShareInfo({
+  //         shareTicket: res.shareTickets[0],
+  //         success: function (res) { console.log(res) },
+  //         fail: function (res) { console.log(res) },
+  //         complete: function (res) { console.log(res) }
+  //       })
+  //     },
+  //     fail(res) {
+  //       // 分享失败
+  //       console.log(res)
+  //     }
+  //   }
+
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
