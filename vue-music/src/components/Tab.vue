5272ad54f2b64b5ae8d09614f6bb3183701aea73
lingyunsheng <1841046698@qq.com>
2019-06-23 23:11:38 +0800

    music

--- vue-music/src/components/Tab.vue
+++ vue-music/src/components/Tab.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tab">
-    <router-link tag="div" class="tab-item" to="/recommend">
+    <router-link tag="div" class="tab-item" to="/recommend" >
       <span class="tab-link">推荐</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/sigle">
