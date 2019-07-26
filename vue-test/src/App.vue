3f92714f7a6161c2c2e7b2971b35b8bb01790307
lingyunsheng <1841046698@qq.com>
2019-07-26 20:31:39 +0800

    vue原理

---
+++ vue-test/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <p>{{count}}</p>
+    <button @click="add(4)">add</button>
+    <btn @click.native="add(4)"></btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      count: 1
+    }
+  },
+  methods: {
+    // 传值
+    add: function (num) {
+      if (num != '') {
+        this.count += num
+      } else {
+        this.count++
+      }
+    }
+  },
+  components: {
+    "btn": {
+      template: `<button>添加</button>`
+    }
+  }
+}
+</script>
+<style>
+</style>
